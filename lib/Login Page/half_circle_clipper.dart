fa199eb39070bd45bcafdc10e6dc4c481e7c0ac6
Affan saleem <m.affansalim@gmail.com>
2023-09-02 00:29:17 +0500

    Second with Shared Preferences added

---
+++ lib/Login Page/half_circle_clipper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/Material.dart';
+
+class HalfCircleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, size.height * 0.5);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height, size.width, size.height * 0.5);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
